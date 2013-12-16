69701148a9eb59bcd45438b773b4fcbc782521e1
Heiko Seeberger <heiko.seeberger@gmail.com>
2013-12-16 17:32:18 +0100

    Add UI

---
+++ src/main/resources/web/js/controllers.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gabblerControllers =
+  angular.module(
+    'gabbler.controllers',
+    [
+      'gabbler.services'
+    ]
+  );
+
+gabblerControllers.controller('HomeCtrl', ['$scope', 'Message', function($scope, Message) {
+
+  $scope.messages = [];
+
+  $scope.getMessages = function() {
+    var messages = Message.query(function() {
+      $scope.messages = messages.concat($scope.messages);
+      $scope.getMessages();
+    });
+  };
+
+  $scope.message = new Message({ 'username': '' });
+
+  $scope.sendMessage = function() {
+    $scope.message.$save();
+    $scope.message = new Message({ 'username': '' });
+  };
+}]);
